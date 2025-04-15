d4c6d5a84edfd7b59cde92617c934dbc7802f256
caglarbaranbora <boracaglarbaran@gmail.com>
2025-04-15 20:23:09 +0300

    'initial'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Promo from "./Promo";
+import Intro1 from "./Intro1";
+import Intro2 from "./Intro2";
+import Intro3 from "./Intro3";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav />
       </header>
+      <body>
+        <Promo />
+        <Intro1 />
+        <Intro2 />
+        <Intro3 />
+      </body>
+      <Footer />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <nav className="main-nav">
+      <ul style={{ listStyle: "none" }}>
+        <li>Home</li>
+        <li>Articles</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
