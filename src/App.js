3ba23665a24aef3e42aebd1b44e302ae3974adb7
caglarbaranbora <boracaglarbaran@gmail.com>
2025-04-16 19:33:51 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Promo from "./components/Promo";
 import Intro from "./components/Intro";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const inputEl = useRef(null);
 
   function handleChange(e) {
     setInputText(e.target.value);
+  }
+
+  function onHandleClick() {
+    inputEl.current.focus();
   }
   return (
     <div className="App">
@@ -18,12 +23,24 @@ function App() {
       </header>
       <div>
         <h1>Konunun basligini giriniz</h1>
-        <input value={inputText} onChange={handleChange} />
+        <input
+          value={inputText}
+          onChange={handleChange}
+          ref={inputEl}
+          type="text"
+        />
         <Intro
           description="Bu konu Edison ile Tesla'dan bahsedecektir"
           header={inputText}
         />
-        <button onClick={() => setInputText("")}>Reset</button>
+        <button
+          onClick={() => {
+            setInputText("");
+            onHandleClick();
+          }}
+        >
+          Reset
+        </button>
         <Promo />
       </div>
       <Footer />
